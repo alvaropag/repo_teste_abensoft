d93462cd5f85b0603c6192101846472b77a76466
Larissa Bedin <larissa.bedin@abensoft.com.br>
2021-09-13 15:14:39 -0300

    primeira parte do arquivo de clientes finalizado >.<

--- larissa/menu_cliente.erl
+++ larissa/menu_cliente.erl
@@ -7,16 +7,16 @@ menu() ->
     menu(Clients).
 
 menu(Clients) ->
-    io:format("============================~n"),
-    io:format(" >>>> WELCOME !  <<<<<~n"),
-    io:format("> Type 1 to Register~n"),
-    io:format("> type 2 to Edit~n"),
-    io:format("> type 3 to Delete~n"),
-    io:format("> type 4 to List~n"),
-    io:format("> type 0 to Quit.~n"),
-    io:format("==========================~n"),
+    io:format("===============================~n"),
+    io:format("-      >>> WELCOME !  <<<    ||~n"),
+    io:format("-> Type 1 to Register        ||~n"),
+    io:format("-> type 2 to Edit            ||~n"),
+    io:format("-> type 3 to Delete          ||~n"),
+    io:format("-> type 4 to List            ||~n"),
+    io:format("-> type 0 to Quit.           ||~n"),
+    io:format("===============================~n"),
 
-    Options = list_to_integer(cadastros:take_value(":")),
+    Options = list_to_integer(cadastros:take_value("=")),
     option(Options, Clients).
 
 option(1, Clients) ->
@@ -24,19 +24,20 @@ option(1, Clients) ->
     menu(Return);
 
 option(2, Clients) ->
-    Id = list_to_integer(take_value("Type the Id to edit:"),
+    Id = cadastros:valida_int(cadastros:take_value("Type the Id to edit:")),
     Return = cadastros:edit_client(Clients, Id),
     menu(Return);
 
-option(3, Clients) ->
-    Id = list_to_integer(take_value("Type the Id to delete:"),
-    Return = cadastros:delete_client(Clients),
+option(3, Client) ->
+    Id = list_to_integer(cadastros:take_value("Type the Id to delete:")),
+    Return = cadastros:delete_client(Client, Id),
     menu(Return);
 
 option(4, Clients) ->
+    io:format("<<< CLIENTS LIST >>>"),
     Return = cadastros:list_clients(Clients),
-   menu(Return);
+    menu(Return);
 
-option(0) ->
-    io:format("'-'").
+option(0, _clients) ->
+    io:format("-_-~n").
 
